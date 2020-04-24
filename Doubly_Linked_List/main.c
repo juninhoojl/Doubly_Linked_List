d5f06df19b5828b25fde887b73d83081e70c53a6
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-04-24 17:11:34 +0200

    Initial Commit

---
+++ Doubly_Linked_List/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Doubly_Linked_List
+//
+//  Created by José Luiz Junior on 4/24/20.
+//  Copyright © 2020 José Luiz Junior. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
