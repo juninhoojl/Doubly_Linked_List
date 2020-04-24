1bcd31176c8e854636c99c4f30a46a055e1780e0
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-04-24 21:03:44 +0200

    Inicial

--- Doubly_Linked_List/main.c
+++ Doubly_Linked_List/main.c
@@ -7,9 +7,136 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+  
+// A linked list node
+typedef struct Node {
+    int data;
+    struct Node * next;
+    struct Node * prev;
+}node;
+
+typedef struct Hnode {
+    int tam;
+    struct Node * primeiro;
+    struct Node * ultimo;
+}hnode;
+
+
+void insere_inicio(hnode * cabeca, int valor){
+
+    node * novo = (node*)malloc(sizeof(node));
+  
+    novo->data = valor;
+    novo->prev = NULL;
+    novo->next = cabeca->primeiro;
+    
+    if (cabeca->primeiro){
+        cabeca->primeiro->prev = novo;
+    }
+    
+    if (cabeca->tam == 0){
+        cabeca->ultimo = novo;
+    }
+    
+    cabeca->primeiro = novo;
+    cabeca->tam+=1;
+    
+}
+
+
+void insere_fim(hnode * cabeca, int valor){
+    
+    node * novo = (node*)malloc(sizeof(node));
+    
+    novo->data = valor;
+    novo->next = NULL;
+    novo->prev = cabeca->ultimo;
+    
+    if (cabeca->ultimo){
+        cabeca->ultimo->next = novo;
+    }
+    
+    if (cabeca->tam == 0){
+        cabeca->primeiro = novo;
+    }
+    
+    cabeca->ultimo = novo;
+    cabeca->tam+=1;
+    
+    return;
+}
+
+void mostra_lista(hnode * cabeca){
+    
+    struct Node * atual = cabeca->primeiro;
+    
+    printf("Lista tamanho = %d\n", cabeca->tam);
+
+    printf("Normal:\n");
+    while(atual){
+        printf("%d ", atual->data);
+        atual = atual->next;
+    }
+    
+    atual = cabeca->ultimo;
+    
+    printf("\nInvertido:\n");
+    while(atual){
+        printf("%d ", atual->data);
+        atual = atual->prev;
+    }
+
+    printf("\n");
+    
+    return;
+}
+
+
+node * busca_node(hnode * cabeca, int valor){
+    
+    struct Node * atual = cabeca->primeiro;
+
+    while(atual && atual->data!=valor){
+        atual = atual->next;
+    }
+
+    if(atual){
+        return atual;
+    }
+    
+    return NULL;
+}
+
+
+
+hnode * inicia_lista(){
+    hnode * novo = (hnode *)malloc(sizeof(hnode));
+    novo->primeiro = NULL;
+    novo->ultimo = NULL;
+    novo->tam = 0;
+    return novo;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    hnode * lista = inicia_lista();
+    
+    printf("Tamanho inicial = %d\n",lista->tam);
+    
+    int i = 0;
+    
+    
+    while(i++<10)<%
+        insere_inicio(lista, i);
+    %>
+    
+    mostra_lista(lista);
+    insere_fim(lista, 11);
+    mostra_lista(lista);
+    
+    printf("Tamanho = %d\n",busca_node(lista, 8)->next->data);
+    
+
     return 0;
 }
